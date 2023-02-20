6f00742973f94afadd036474bce61e249ccccba4
Alex Rocha <rocha.amc@gmail.com>
2023-02-19 16:17:43 -0800

    Add authentication

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
 # Create your models here.
+from app.extensions import db
+from sqlalchemy.orm import backref
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"<User: {self.username}>"
