dd8b0fbc05f6c2c4dae56d551407073387f991e6
Alex Rocha <rocha.amc@gmail.com>
2023-02-25 16:14:26 -0800

    Add contact detail

--- intouch_app/main/routes.py
+++ intouch_app/main/routes.py
@@ -14,7 +14,8 @@ main = Blueprint('main', __name__)
 @login_required
 def homepage():
     """Show homepage."""
-    return render_template('home.html')
+    all_contacts = Contact.query.filter_by(user_id=current_user.id).all()
+    return render_template('home.html', contacts=all_contacts)
 
 @main.route('/create_contact', methods=['GET', 'POST'])
 @login_required
@@ -37,3 +38,27 @@ def create_contact():
         flash('New contact was created successfully.')
         return redirect(url_for('main.contact_detail', contact_id=new_contact.id))
     return render_template('create_contact.html', form=form)
+
+@main.route('/contact/<contact_id>', methods=['GET', 'POST'])
+@login_required
+def contact_detail(contact_id):
+    contact = Contact.query.get(contact_id)
+    if (contact is None or contact.user_id != current_user.id):
+        return redirect(url_for('main.homepage'))
+
+    form = ContactForm(obj=contact)
+
+    # if form was submitted and contained no errors
+    if form.validate_on_submit():
+        contact.name = form.name.data
+        contact.email = form.email.data
+        contact.phone = form.phone.data
+        contact.address = form.address.data
+        contact.birthday = form.birthday.data
+
+        db.session.commit()
+
+        flash('Contact was updated successfully.')
+        return redirect(url_for('main.contact_detail', contact_id=contact_id))
+
+    return render_template('contact_detail.html', contact=contact, form=form)
